24957a15d3b4156f9d5f0250ac4566b0864502a4
SohamPawgi15 <spawgi@asu.edu>
2024-07-22 23:40:41 -0700

    Added the last remaining 5 practice questions and finished the practice data analysis project 1.

--- weather.py
+++ weather.py
@@ -55,3 +55,30 @@ print(data[data['Weather']=='Snow'])
 # Q.10) Find all instances when 'Wind speed is above 24' and 'Visibility is 25'
 print("\n Answer for Q.10: ")
 print(data[(data['Wind Speed_km/h']>24) & (data['Visibility_km'] == 25)])
+
+# Q.11) What is the mean value of each column against each 'Weather'?
+print("\n Answer for Q.11: ")
+mean_value = data.groupby('Weather').mean(numeric_only=True)
+print(mean_value)
+
+# Q.12) What is the minimum and maximum value of each column against each 'Weather'?
+print("\n Answer for Q.12: ")
+min_value = data.groupby('Weather').min(numeric_only=True)
+max_value = data.groupby('Weather').max(numeric_only=True)
+print('\n Min values:\n', min_value)
+print('\n Max values:\n', max_value)
+
+# Q.13) Show all the records where the 'Weather' is Fog.
+print("\n Answer for Q.13: ")
+fog_values = data[data['Weather']=='Fog']
+print(fog_values)
+
+# Q.14) Find all instances when 'Weather is clear' or 'Visibility is above 40'
+print("\n Answer for Q.14: ")
+clear_40 = data[(data['Weather']=='Clear') | (data['Visibility_km']>40)]
+print(clear_40)
+
+# Q.15) Find all instances when: 'Weather is clear' and 'Relative humidity is greater than 50' OR 'Visibility is above 40'
+print("\n Answer for Q.15: ")
+clear_50_40 = data[(data['Weather']=='Clear') & (data['Rel Hum_%']>50) | (data['Visibility_km']>40)]
+print(clear_50_40)
